b18cb03a550eb9058ec0cdb26a375852c7bf1118
50134830-sudo <50134830@est.ort.edu.ar>
2026-05-04 10:12:25 -0300

    Initial commit

---
+++ Assets/scripts/Interaction_area.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction_area : MonoBehaviour
+{
+    void OnCollisionEnter(Collision.col) 
+    {
+        Debug.Log("colicion");
+    }
+}
